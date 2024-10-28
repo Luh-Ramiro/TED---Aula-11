b26a3ded5696ecdf5822c817e189cf9652983e14
Luh-Ramiro <ramiro.lucivania@gmail.com>
2024-10-28 19:13:36 -0300

    TED

--- Atividade.01.py
+++ Atividade.01.py
@@ -1,13 +1,5 @@
 def encontrar_repetidos(vetor):
-    """
-    Encontra números repetidos em um vetor e suas posições.
-
-    Args:
-        vetor: Um vetor de números inteiros.
-
-    Returns:
-        None
-    """
+    
 
     repetidos = set()
     for i in range(len(vetor)):
@@ -17,7 +9,7 @@ def encontrar_repetidos(vetor):
         else:
             repetidos.add(num)
 
-# Leitura dos 10 números
+
 vetor = []
 for _ in range(10):
     num = int(input("Digite um número: "))
